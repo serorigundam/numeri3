ab15eb5a8e2d25c8163db2e78792fbec7449500e
serori <serori328@gmail.com>
2016-12-01 23:06:32 +0900

    first commit

---
+++ app/src/test/java/net/ketc/app/ExampleSpec.kt
@@ -0,0 +1,17 @@
+package net.ketc.app
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+
+@RunWith(JUnitPlatform::class)
+class ExampleSpec : Spek({
+    describe("a addition") {
+        it("2+2 equals 4") {
+            assertEquals(4, 2 + 2)
+        }
+    }
+})
