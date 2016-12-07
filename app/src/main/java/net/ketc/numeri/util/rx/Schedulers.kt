7d74bfabdb367509821c399879cfa880a905d745
serori <serori328@gmail.com>
2016-12-07 19:41:55 +0900

    パッケージインポートしてるのにパッケージ辿ってメソッドコールしてた

--- app/src/main/java/net/ketc/numeri/util/rx/Schedulers.kt
+++ app/src/main/java/net/ketc/numeri/util/rx/Schedulers.kt
@@ -4,8 +4,8 @@ import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.Executors
 
 object MySchedulers {
-    val stream = io.reactivex.schedulers.Schedulers.from(Executors.newFixedThreadPool(1))!!
-    val io = io.reactivex.schedulers.Schedulers.from(Executors.newFixedThreadPool(1))!!
-    val imageLoad = io.reactivex.schedulers.Schedulers.from(Executors.newFixedThreadPool(10))!!
-    val twitter = io.reactivex.schedulers.Schedulers.from(Executors.newFixedThreadPool(10))!!
+    val stream = Schedulers.from(Executors.newFixedThreadPool(1))!!
+    val io = Schedulers.from(Executors.newFixedThreadPool(1))!!
+    val imageLoad = Schedulers.from(Executors.newFixedThreadPool(10))!!
+    val twitter = Schedulers.from(Executors.newFixedThreadPool(10))!!
 }
