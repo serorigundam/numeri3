7486c09b034353cadc6ae02f91eaff5095f27e5c
serori <serori328@gmail.com>
2016-12-16 15:38:24 +0900

    ドキュメントの追加とか

--- app/src/main/kotlin/net/ketc/numeri/presentation/view/ApplicationActivity.kt
+++ app/src/main/kotlin/net/ketc/numeri/presentation/view/ApplicationActivity.kt
@@ -1,11 +1,12 @@
 package net.ketc.numeri.presentation.view
 
 import android.content.Context
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import net.ketc.numeri.presentation.presenter.Presenter
 
 /**
- * @param P
+ * @param P is bound [Presenter]
  */
 abstract class ApplicationActivity<P : Presenter<ActivityInterface>> : AppCompatActivity() {
     /**
@@ -16,6 +17,16 @@ abstract class ApplicationActivity<P : Presenter<ActivityInterface>> : AppCompat
     override fun onPause() {
         super.onPause()
         presenter.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        presenter.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        presenter.onRestoreInstanceState(savedInstanceState)
     }
 
     override fun onResume() {
