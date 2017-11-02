7ac228c170afeea288535ae9ef6bc1c9349cb260
serori <serori328@gmail.com>
2017-11-02 14:54:20 +0900

    ツイート送信の進捗を通知に表示する機能を実装

--- app/src/main/kotlin/net/ketc/numeri/util/twitter/TweetSender.kt
+++ app/src/main/kotlin/net/ketc/numeri/util/twitter/TweetSender.kt
@@ -1,8 +1,10 @@
 package net.ketc.numeri.util.twitter
 
+import io.reactivex.Observable
 import net.ketc.numeri.domain.service.TwitterClient
 import twitter4j.StatusUpdate
 import java.io.File
+import java.sql.ParameterMetaData
 import kotlin.coroutines.experimental.buildSequence
 
 class TweetSender(private val client: TwitterClient) {
@@ -10,9 +12,24 @@ class TweetSender(private val client: TwitterClient) {
     var inReplyToStatusId: Long? = null
     var mediaList: List<File>? = null
     var isPossiblySensitive = false
+    private var onProgressListener: (Int) -> Unit = {}
+
+    val progressObservable: Observable<Int> by lazy {
+        Observable.create<Int> { emitter ->
+            onProgressListener = { progress ->
+                if (progress == PROGRESS_MAX) {
+                    emitter.onComplete()
+                } else {
+                    emitter.onNext(progress)
+                }
+            }
+        }
+    }
+
     fun send() {
         (inReplyToStatusId ?: 0).takeIf { it >= 0 } ?: throw IllegalStateException("InReplyStatusId must be a positive number")
         (mediaList?.size ?: 0).takeIf { it <= 4 } ?: throw IllegalStateException("The size of mediaList must be 4 or less")
+        onProgressListener(0)
         val statusUpdate = StatusUpdate(text).apply {
             this@TweetSender.inReplyToStatusId?.let {
                 inReplyToStatusId = it
@@ -22,24 +39,32 @@ class TweetSender(private val client: TwitterClient) {
             }
         }
         client.twitter.updateStatus(statusUpdate)
+        onProgressListener(PROGRESS_MAX)
     }
 
     private fun uploadMedia(mediaList: List<File>) = buildSequence {
+        val diff = 80 / mediaList.size
+        var progress = 0
         mediaList.forEach {
             yield(client.twitter.uploadMedia(it).mediaId)
+            progress += diff
+            onProgressListener(progress)
         }
+    }
+
+    companion object {
+        private val PROGRESS_MAX = 100
     }
 }
 
-fun TwitterClient.sendTweet(text: String = "",
-                            inReplyToStatusId: Long? = null,
-                            mediaList: List<File>? = null,
-                            isPossiblySensitive: Boolean = false) {
-    TweetSender(this).run {
+fun TwitterClient.createTweetSender(text: String = "",
+                                    inReplyToStatusId: Long? = null,
+                                    mediaList: List<File>? = null,
+                                    isPossiblySensitive: Boolean = false): TweetSender {
+    return TweetSender(this).apply {
         this.text = text
         this.inReplyToStatusId = inReplyToStatusId
         this.mediaList = mediaList
         this.isPossiblySensitive = isPossiblySensitive
-        send()
     }
 }
