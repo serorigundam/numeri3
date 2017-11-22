c80ee8c772f01125df6466c8b71b88f679ee55d8
serori <serori328@gmail.com>
2017-11-23 08:14:17 +0900

    排他制御を修正

--- app/src/main/kotlin/tech/ketc/numeri/domain/repository/ImageRepository.kt
+++ app/src/main/kotlin/tech/ketc/numeri/domain/repository/ImageRepository.kt
@@ -18,10 +18,11 @@ import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import tech.ketc.numeri.domain.model.BitmapContent
 import tech.ketc.numeri.infra.element.MimeType
+import tech.ketc.numeri.util.Logger
+import tech.ketc.numeri.util.logTag
 import java.io.*
-import java.util.concurrent.locks.ReentrantReadWriteLock
-import kotlin.concurrent.read
-import kotlin.concurrent.write
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 
 class ImageRepository @Inject constructor(private val mApp: App, private val mDatabase: ImageDatabase) : IImageRepository {
@@ -33,9 +34,9 @@ class ImageRepository @Inject constructor(private val mApp: App, private val mDa
 
     private val mDao = mDatabase.imageDao()
 
-    private val mCacheLock = ReentrantReadWriteLock()
-    private val mDatabaseLock = ReentrantReadWriteLock()
-
+    private val mDownloadLock = LinkedHashMap<String, ReentrantLock>()
+    private val mCacheWriteLock = LinkedHashMap<String, ReentrantLock>()
+    private val mDatabaseLock = LinkedHashMap<String, ReentrantLock>()
 
     private val bitmapCache = object : LruCache<String, BitmapContent>(MAX) {
         override fun sizeOf(key: String, value: BitmapContent): Int {
@@ -43,25 +44,52 @@ class ImageRepository @Inject constructor(private val mApp: App, private val mDa
         }
     }
 
-    override fun downloadOrGet(urlStr: String, cache: Boolean)
-            = localGet(urlStr) ?: download(urlStr, cache)
+    private fun <R> downloadLock(urlStr: String, handle: () -> R): R {
+        val lock = mDownloadLock[urlStr]
+        lock ?: return handle()
+        return lock.withLock(handle)
+    }
+
+    private fun <R> cacheWriteLock(urlStr: String, handle: () -> R): R {
+        val lock = mCacheWriteLock[urlStr]
+        lock ?: return handle()
+        return lock.withLock(handle)
+    }
+
+    private fun <R> databaseLock(urlStr: String, handle: () -> R): R {
+        val lock = mDatabaseLock[urlStr]
+        lock ?: return handle()
+        return lock.withLock(handle)
+    }
+
+
+    override fun downloadOrGet(urlStr: String, cache: Boolean) = downloadLock(urlStr) {
+        localGet(urlStr) ?: download(urlStr, cache)
+    }
 
     private fun localGet(urlStr: String): BitmapContent? {
         fun getForDB(): BitmapContent? {
-            val image = mDatabaseLock.read {
-                mDao.selectById(urlStr)
-            } ?: return null
+            val image = mDao.selectById(urlStr)
+                    ?: return null
             val data = image.pngData
             val mimeType = image.mimeType
             val bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
             return BitmapContent(bitmap, mimeType).also {
-                mCacheLock.write { bitmapCache.put(urlStr, it) }
+                cache(urlStr, it)
             }
         }
-        return mCacheLock.read { bitmapCache[urlStr] } ?: getForDB()
+        return cacheWriteLock(urlStr) { bitmapCache[urlStr] } ?: databaseLock(urlStr) { getForDB() }
+    }
+
+    private fun cache(urlStr: String, content: BitmapContent) {
+        mCacheWriteLock.put(urlStr, ReentrantLock())
+        bitmapCache.put(urlStr, content)
+        Logger.v(logTag, "cache $urlStr")
+        mCacheWriteLock.remove(urlStr)
     }
 
     private fun download(urlStr: String, cache: Boolean): BitmapContent {
+        mDownloadLock.put(urlStr, ReentrantLock())
         var connection: HttpURLConnection? = null
         var inputStream: InputStream? = null
         return try {
@@ -77,6 +105,7 @@ class ImageRepository @Inject constructor(private val mApp: App, private val mDa
                 val contentType = connection.contentType.toLowerCase()
                 val bitmap = BitmapFactory.decodeStream(inputStream)
                 val mimeType = mimeType(contentType)
+                Logger.v(logTag, "download $urlStr")
                 BitmapContent(bitmap, mimeType).also {
                     if (!cache) return@also
                     saveLocalDatabase(urlStr, it)
@@ -87,6 +116,7 @@ class ImageRepository @Inject constructor(private val mApp: App, private val mDa
         } finally {
             inputStream?.close()
             connection?.disconnect()
+            mDownloadLock.remove(urlStr)
         }
     }
 
@@ -103,19 +133,19 @@ class ImageRepository @Inject constructor(private val mApp: App, private val mDa
 
 
     private fun saveLocalDatabase(urlStr: String, content: BitmapContent) {
-        mCacheLock.write { bitmapCache.put(urlStr, content) }
+        cache(urlStr, content)
         launch {
+            mDatabaseLock.put(urlStr, ReentrantLock())
+            Logger.v(logTag, "save local db $urlStr")
             val byteArray = async(coroutineContext + CommonPool) {
                 ByteArrayOutputStream().use {
                     content.bitmap.compress(content.mimeType.toFormat(), 50, it)
                     it.toByteArray()
                 }
             }.await()
-
-            mDatabaseLock.write {
-                if (mDao.count() == LOCAL_RECORD_MAX) mDao.deleteOldest()
-                mDao.insert(Image(urlStr, byteArray, content.mimeType))
-            }
+            if (mDao.count() == LOCAL_RECORD_MAX) mDao.deleteOldest()
+            mDao.insert(Image(urlStr, byteArray, content.mimeType))
+            mDatabaseLock.remove(urlStr)
         }
     }
 
