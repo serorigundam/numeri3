992e1d65fb8d7334071dd1d3b0b1c211f01bdf92
serori <serori328@gmail.com>
2017-11-28 11:37:45 +0900

    ankoViewつかえばええやん！！！

--- app/src/main/kotlin/tech/ketc/numeri/ui/view/pager/NonScrollableViewPager.kt
+++ app/src/main/kotlin/tech/ketc/numeri/ui/view/pager/NonScrollableViewPager.kt
@@ -2,12 +2,10 @@ package tech.ketc.numeri.ui.view.pager
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.support.v4.view.ViewPager
-import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
+import org.jetbrains.anko.support.v4._ViewPager
 
-class NonScrollableViewPager(context: Context) : ViewPager(context) {
+class NonScrollableViewPager(context: Context) : _ViewPager(context) {
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
         return false
     }
@@ -15,34 +13,5 @@ class NonScrollableViewPager(context: Context) : ViewPager(context) {
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent?): Boolean {
         return false
-    }
-
-    inline fun <T : View> T.lparams(init: ViewPager.LayoutParams.() -> Unit): T {
-        val layoutParams = ViewPager.LayoutParams()
-        layoutParams.init()
-        this@lparams.layoutParams = layoutParams
-        return this
-    }
-
-    fun <T : View> T.lparams(): T {
-        val layoutParams = ViewPager.LayoutParams()
-        this@lparams.layoutParams = layoutParams
-        return this
-    }
-
-    inline fun <T : View> T.lparams(context: Context?,
-                                    attrs: AttributeSet?,
-                                    init: ViewPager.LayoutParams.() -> Unit): T {
-        val layoutParams = ViewPager.LayoutParams(context!!, attrs!!)
-        layoutParams.init()
-        this@lparams.layoutParams = layoutParams
-        return this
-    }
-
-    fun <T : View> T.lparams(context: Context?,
-                             attrs: AttributeSet?): T {
-        val layoutParams = ViewPager.LayoutParams(context!!, attrs!!)
-        this@lparams.layoutParams = layoutParams
-        return this
     }
 }
--- app/src/main/kotlin/tech/ketc/numeri/ui/view/pager/_NonScrollableViewPager.kt
+++ app/src/main/kotlin/tech/ketc/numeri/ui/view/pager/_NonScrollableViewPager.kt
@@ -1,14 +1,11 @@
 package tech.ketc.numeri.ui.view.pager
 
 import android.support.v4.view.ViewPager
-import android.view.ViewGroup
 import android.view.ViewManager
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.AnkoException
-import org.jetbrains.anko.AnkoViewDslMarker
+import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.support.v4._ViewPager
 
-fun ViewManager.nonScrollableViewPager(init: (@AnkoViewDslMarker ViewPager).() -> Unit) = when (this) {
-    is ViewGroup -> NonScrollableViewPager(context).apply(init).also { addView(it) }
-    is AnkoContext<*> -> NonScrollableViewPager(ctx).apply(init).also { addView(it, null) }
-    else -> throw AnkoException("$this is the wrong parent")
+
+inline fun ViewManager.nonScrollableViewPager(theme: Int = 0, init: _ViewPager.() -> Unit): ViewPager {
+    return ankoView(::NonScrollableViewPager, theme, init)
 }
